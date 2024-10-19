e50e950e8e842798122766f194bee5053a962376
khoavt <thekhoa2206@gmail.com>
2024-10-19 21:56:06 +0700

    push

--- src/main/java/com/psn/common/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/psn/common/security/JwtAuthenticationEntryPoint.java
@@ -1,73 +1,73 @@
-package com.psn.common.security;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.JWSVerifier;
-import com.nimbusds.jose.crypto.MACVerifier;
-import com.nimbusds.jwt.SignedJWT;
-import com.psn.common.exception.ApiException;
-import com.psn.common.exception.ApiResponse;
-import com.psn.common.exception.ErrorCode;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.experimental.NonFinal;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-
-import java.io.IOException;
-import java.text.ParseException;
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
-
-public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    @NonFinal
-    @Value("${jwt.signerKey}")
-    protected String SIGNER_KEY;
-
-    @NonFinal
-    @Value("${jwt.valid-duration}")
-    protected long VALID_DURATION;
-
-    @NonFinal
-    @Value("${jwt.refreshable-duration}")
-    protected long REFRESHABLE_DURATION;
-    @Override
-    public void commence(
-            HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
-            throws IOException {
-        ErrorCode errorCode = ErrorCode.UNAUTHENTICATED;
-
-        response.setStatus(errorCode.getStatusCode().value());
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-
-        ApiResponse<?> apiResponse = ApiResponse.builder()
-                .code(errorCode.getCode())
-                .message(errorCode.getMessage())
-                .build();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
-        response.flushBuffer();
-    }
-
-    private SignedJWT verifyToken(String token, boolean isRefresh) throws JOSEException, ParseException {
-        JWSVerifier verifier = new MACVerifier(SIGNER_KEY.getBytes());
-
-        SignedJWT signedJWT = SignedJWT.parse(token);
-
-        Date expiryTime = (isRefresh)
-                ? new Date(signedJWT.getJWTClaimsSet().getIssueTime()
-                .toInstant().plus(REFRESHABLE_DURATION, ChronoUnit.SECONDS).toEpochMilli())
-                : signedJWT.getJWTClaimsSet().getExpirationTime();
-
-        var verified = signedJWT.verify(verifier);
-
-        if (!(verified && expiryTime.after(new Date()))) throw new ApiException(ErrorCode.UNAUTHENTICATED);
-
-
-        return signedJWT;
-    }
-}
+//package com.psn.common.security;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.nimbusds.jose.JOSEException;
+//import com.nimbusds.jose.JWSVerifier;
+//import com.nimbusds.jose.crypto.MACVerifier;
+//import com.nimbusds.jwt.SignedJWT;
+//import com.psn.common.exception.ApiException;
+//import com.psn.common.exception.ApiResponse;
+//import com.psn.common.exception.ErrorCode;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.experimental.NonFinal;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.http.MediaType;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.web.AuthenticationEntryPoint;
+//
+//import java.io.IOException;
+//import java.text.ParseException;
+//import java.time.temporal.ChronoUnit;
+//import java.util.Date;
+//
+//public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+//    @NonFinal
+//    @Value("${jwt.signerKey}")
+//    protected String SIGNER_KEY;
+//
+//    @NonFinal
+//    @Value("${jwt.valid-duration}")
+//    protected long VALID_DURATION;
+//
+//    @NonFinal
+//    @Value("${jwt.refreshable-duration}")
+//    protected long REFRESHABLE_DURATION;
+//    @Override
+//    public void commence(
+//            HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+//            throws IOException {
+//        ErrorCode errorCode = ErrorCode.UNAUTHENTICATED;
+//
+//        response.setStatus(errorCode.getStatusCode().value());
+//        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+//
+//        ApiResponse<?> apiResponse = ApiResponse.builder()
+//                .code(errorCode.getCode())
+//                .message(errorCode.getMessage())
+//                .build();
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//
+//        response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
+//        response.flushBuffer();
+//    }
+//
+//    private SignedJWT verifyToken(String token, boolean isRefresh) throws JOSEException, ParseException {
+//        JWSVerifier verifier = new MACVerifier(SIGNER_KEY.getBytes());
+//
+//        SignedJWT signedJWT = SignedJWT.parse(token);
+//
+//        Date expiryTime = (isRefresh)
+//                ? new Date(signedJWT.getJWTClaimsSet().getIssueTime()
+//                .toInstant().plus(REFRESHABLE_DURATION, ChronoUnit.SECONDS).toEpochMilli())
+//                : signedJWT.getJWTClaimsSet().getExpirationTime();
+//
+//        var verified = signedJWT.verify(verifier);
+//
+//        if (!(verified && expiryTime.after(new Date()))) throw new ApiException(ErrorCode.UNAUTHENTICATED);
+//
+//
+//        return signedJWT;
+//    }
+//}
--- src/main/java/com/psn/common/security/SecurityConfiguration.java
+++ src/main/java/com/psn/common/security/SecurityConfiguration.java
@@ -1,48 +1,48 @@
-package com.psn.common.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
-public class SecurityConfiguration {
-    private static final String[] PUBLIC_ENDPOINTS = {
-            "/api/v1/users", "/api/v1/auth/token", "/auth-service/api/v1/auth/authenticate", "/api/v1/auth/logout", "/api/v1/auth/refresh"
-    };
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS)
-                .permitAll()
-                .anyRequest()
-                .authenticated());
-
-        return httpSecurity.build();
-    }
-
-    @Bean
-    JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
-
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
-
-        return jwtAuthenticationConverter;
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
-    }
-}
+//package com.psn.common.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+//import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity
+//public class SecurityConfiguration {
+//    private static final String[] PUBLIC_ENDPOINTS = {
+//            "/api/v1/users", "/api/v1/auth/token", "/auth-service/api/v1/auth/authenticate", "/api/v1/auth/logout", "/api/v1/auth/refresh"
+//    };
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS)
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated());
+//
+//        return httpSecurity.build();
+//    }
+//
+//    @Bean
+//    JwtAuthenticationConverter jwtAuthenticationConverter() {
+//        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+//        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
+//
+//        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+//        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+//
+//        return jwtAuthenticationConverter;
+//    }
+//
+//    @Bean
+//    PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder(10);
+//    }
+//}
