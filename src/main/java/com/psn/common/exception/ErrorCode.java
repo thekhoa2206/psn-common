8e37d8136d62d38b50571ae9b94dda4ec80263f2
khoavt <thekhoa2206@gmail.com>
2024-10-19 21:19:49 +0700

    update lib

--- src/main/java/com/psn/common/exception/ErrorCode.java
+++ src/main/java/com/psn/common/exception/ErrorCode.java
@@ -16,6 +16,7 @@ public enum ErrorCode {
     PARAMS_NOT_NULL(1009, "Param is not null", HttpStatus.BAD_REQUEST),
     SAVE_NOT_SUCCESS(1010, "Save data not success", HttpStatus.INTERNAL_SERVER_ERROR),
     ID_NOT_NULL(1011, "Id not null", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED(1012, "User not existed", HttpStatus.BAD_REQUEST)
     ;
 
     ErrorCode(int code, String message, HttpStatusCode statusCode) {
