54bffbea10b2a566ff3c4b187c1e68de64fd5085
khoavt <thekhoa2206@gmail.com>
2024-11-10 00:17:06 +0700

    update

--- src/main/java/com/psn/common/security/AutoConfigurationSecurityCommon.java
+++ src/main/java/com/psn/common/security/AutoConfigurationSecurityCommon.java
@@ -1,5 +1,7 @@
 package com.psn.common.security;
 
+import com.psn.common.security.service.AuthService;
+import com.psn.common.security.service.JwtService;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.ElementType;
@@ -9,7 +11,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)  // Annotation này sẽ được áp dụng cho class
 @Retention(RetentionPolicy.RUNTIME)
-@Import({SecurityConfiguration.class, JwtAuthenticationFilter.class})
+@Import({SecurityConfiguration.class, JwtAuthenticationFilter.class, JwtService.class, AuthService.class})
 public @interface AutoConfigurationSecurityCommon {
 
 }
