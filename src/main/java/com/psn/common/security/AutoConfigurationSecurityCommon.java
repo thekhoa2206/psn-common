612355ad26af976c5e5d48a2167f343a3b972823
khoavt <thekhoa2206@gmail.com>
2024-11-12 21:21:00 +0700

    push

--- src/main/java/com/psn/common/security/AutoConfigurationSecurityCommon.java
+++ src/main/java/com/psn/common/security/AutoConfigurationSecurityCommon.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)  // Annotation này sẽ được áp dụng cho class
 @Retention(RetentionPolicy.RUNTIME)
 @Import({SecurityConfiguration.class, JwtAuthenticationFilter.class})
-@ComponentScan(basePackages = "com.psn.common.service")
+@ComponentScan(basePackages = {"com.psn.common.security"})
 public @interface AutoConfigurationSecurityCommon {
 
 }
