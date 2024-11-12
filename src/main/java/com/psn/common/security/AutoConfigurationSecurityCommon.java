1000ca620a997d52aa521eb83f32272d4f06f84e
khoavt <thekhoa2206@gmail.com>
2024-11-12 21:11:58 +0700

    git push

--- src/main/java/com/psn/common/security/AutoConfigurationSecurityCommon.java
+++ src/main/java/com/psn/common/security/AutoConfigurationSecurityCommon.java
@@ -1,5 +1,6 @@
 package com.psn.common.security;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.ElementType;
@@ -10,6 +11,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)  // Annotation này sẽ được áp dụng cho class
 @Retention(RetentionPolicy.RUNTIME)
 @Import({SecurityConfiguration.class, JwtAuthenticationFilter.class})
+@ComponentScan(basePackages = "com.psn.common.service")
 public @interface AutoConfigurationSecurityCommon {
 
 }
